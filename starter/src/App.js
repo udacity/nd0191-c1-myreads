8334712c65716df3006b6208fc8b13e7ea56c648
Zilvinas Marozas <zmarozas@gmail.com>
2022-05-21 15:27:14 -0400

    changes

--- starter/src/App.js
+++ starter/src/App.js
@@ -5,7 +5,7 @@ import Main from "./Main";
 
 function App() {
   const [showSearchPage, setShowSearchpage] = useState(false);
-  const booksArray = [
+  const [booksArray, setBooksArray] = useState([
     {
       url: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
       title: "To Kill a Mockingbird",
@@ -54,7 +54,7 @@ function App() {
       author: "Mark Twain",
       shelf: "read",
     },
-  ];
+  ]);
   return (
     <div className="app">
       {showSearchPage ? (
