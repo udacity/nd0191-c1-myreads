58d9359df870c8d9cf722a1e928e6ceb311af08a
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2022-07-15 23:55:09 +0200

    feat: presist state in localstorage

--- starter/src/context/ShelfContext.js
+++ starter/src/context/ShelfContext.js
@@ -1,15 +1,10 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 //Create Context
 export const ShelfContext = createContext(null);
 // DIY Provider
 export const ShelfProvider = ({ children }) => {
-  // const IntState = {
-  //   read: new Set(),
-  //   wantToRead: new Set(),
-  //   currentlyReading: new Set(),
-  // };
-  const IntState = {
+  const IntState = JSON.parse(localStorage.getItem("myReadsStorage")) || {
     read: [],
     wantToRead: [],
     currentlyReading: [],
@@ -17,32 +12,11 @@ export const ShelfProvider = ({ children }) => {
   const [state, setState] = useState(IntState);
   const api = {
     stateShelfUpdate: (data) => setState(data),
-    // addTo: (target = "read", bookId) =>
-    //   setState((prev) => {
-    //     console.log(
-    //       "target :   " + target + " bookId   : " + JSON.stringify(bookId)
-    //     );
-    //     let next = { ...prev };
-    //     //  fist rest bookId shelf
-    //     console.log("reads :", prev.read.has(bookId));
-    //     console.log("currentlyReading :", prev.currentlyReading.has(bookId));
-    //     console.log("wantToRead", prev.wantToRead.has(bookId));
-
-    //     if (
-    //       prev.read.has(bookId) ||
-    //       prev.currentlyReading.has(bookId) ||
-    //       prev.wantToRead.has(bookId)
-    //     ) {
-    //       console.log("here");
-    //       next.read.has(bookId) && next.read.delete(bookId);
-    //       next.currentlyReading.has(bookId) &&
-    //         next.currentlyReading.delete(bookId);
-    //       next.wantToRead.has(bookId) && next.wantToRead.delete(bookId);
-    //     }
-    //     next[target]?.add(bookId);
-    //     return { ...prev, ...next };
-    //   }),
   };
+
+  useEffect(() => {
+    localStorage.setItem("myReadsStorage", JSON.stringify(state));
+  }, [state]);
 
   return (
     <ShelfContext.Provider value={{ state, ...api }}>
