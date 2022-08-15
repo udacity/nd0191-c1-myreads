b78e0df4bbaea61f25f6ebc978aba27121c855cd
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-08-15 19:21:58 +0100

    feat: Implemented BooksAPI logic for getAll and checked its coming through console.log

--- starter/src/components/App.js
+++ starter/src/components/App.js
@@ -1,15 +1,30 @@
+import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import "../css/App.css";
+import * as BooksAPI from "../BooksAPI.js";
 import BookLibrary from "./BookLibrary.js";
 import SearchBooks from "./SearchBooks.js";
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  console.log("app books", books);
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const res = await BooksAPI.getAll();
+      setBooks(res);
+    };
+
+    getBooks();
+  }, []);
+
   return (
     <div className="app">
       <Routes>
         <Route exact path="/" element={<BookLibrary />} />
-        <Route path="/search" element={<SearchBooks />} />
+        <Route path="/search" element={<SearchBooks books={books} />} />
       </Routes>
     </div>
   );
