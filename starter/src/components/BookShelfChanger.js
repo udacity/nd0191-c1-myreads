ca528dcf9996af850bbe5e22cd14f0e80984858e
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-08-15 16:26:47 +0100

    refactor: Created component for 'BookShelfChanger' and replaced this in the starter code in App.js file to confer it appears as expected

---
+++ starter/src/components/BookShelfChanger.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const BookShelfChanger = () => {
+  const [shelf, setShelf] = useState("none");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setShelf(e.target.value);
+  };
+
+  return (
+    <div className="book-shelf-changer">
+      <select value={shelf} onChange={handleChange}>
+        <option value="none" disabled>
+          Move to...
+        </option>
+        <option value="currentlyReading">Currently Reading</option>
+        <option value="wantToRead">Want to Read</option>
+        <option value="read">Read</option>
+        <option value="none">None</option>
+      </select>
+    </div>
+  );
+};
+
+export default BookShelfChanger;
