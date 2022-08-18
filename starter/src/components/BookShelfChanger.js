805706c9b07853b549a5d234b782938872cdf35e
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-08-18 19:12:48 +0100

    feat: Add colour differentiation in book dropdown to show which shelf book is on

--- starter/src/components/BookShelfChanger.js
+++ starter/src/components/BookShelfChanger.js
@@ -1,24 +1,45 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 
+const shelfOptions = [
+  { value: "currentlyReading", label: "Currently Reading" },
+  { value: "wantToRead", label: "Want to Read" },
+  { value: "read", label: "Read" },
+  { value: "none", label: "None" },
+];
+
 const BookShelfChanger = ({ book, bookShelfHandler }) => {
-  const [shelf, setShelf] = useState("none");
+  const [shelf, setShelf] = useState(book.shelf);
 
   const handleChange = (e) => {
+    console.log("shelf", e.target.value);
+    console.log("b shelf", book.shelf);
     setShelf(e.target.value);
     bookShelfHandler(book, e.target.value);
   };
 
+  console.log("shelf o", shelf);
   return (
     <div className="book-shelf-changer">
-      <select value={shelf} onChange={handleChange}>
+      <select value={shelf} onChange={(e) => handleChange(e)}>
         <option value="none" disabled>
           Move to...
         </option>
-        <option value="currentlyReading">Currently Reading</option>
-        <option value="wantToRead">Want to Read</option>
-        <option value="read">Read</option>
-        <option value="none">None</option>
+        {shelfOptions.map((option, idx) =>
+          book.shelf === option.value ? (
+            <option
+              className={"selected-option"}
+              key={idx}
+              value={option.value}
+            >
+              {option.label}
+            </option>
+          ) : (
+            <option key={idx} value={option.value}>
+              {option.label}
+            </option>
+          )
+        )}
       </select>
     </div>
   );
