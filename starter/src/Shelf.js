1466bfeca79df8ee79c43aff5f5a465b6f5107cc
Douglas E Scott <dscott427@msn.com>
2022-10-04 13:28:34 -0400

    Set-up

---
+++ starter/src/Shelf.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function Shelf() {
+  const [showSearchPage, setShowSearchpage] = useState(false);
+
+  return (
+    <div></div>
+  );
+}
+
+export default Shelf;
