a60d90e1e9924262167649d72856f55031545e1b
Douglas E Scott <dscott427@msn.com>
2022-10-09 02:25:59 -0400

    Update Book.js

--- starter/src/Book.js
+++ starter/src/Book.js
@@ -15,8 +15,6 @@ const Book = ({book, updateBook}) => {
                               width: 128,
                               height: 188,
                               backgroundImage:
-                                'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
-                            }}
                                 `url(${book.imageLinks.thumbnail})`,
                             }}                           
                           ></div>
