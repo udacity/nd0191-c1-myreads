6b7dc7a1126aaaa96311b73d7a6d6982845599da
drichards188 <github@cxoprotect.com>
2022-10-10 10:43:02 -0600

    bookshelves dynamically update

--- starter/src/components/Book.js
+++ starter/src/components/Book.js
@@ -1,4 +1,4 @@
-const Book = () => {
+const Book = ({bookData}) => {
 
     return (
         <div className="book">
@@ -9,7 +9,7 @@ const Book = () => {
                         width: 128,
                         height: 193,
                         backgroundImage:
-                            'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
+                            `url(${bookData.imageLinks.thumbnail})`,
                     }}
                 ></div>
                 <div className="book-shelf-changer">
@@ -26,8 +26,8 @@ const Book = () => {
                     </select>
                 </div>
             </div>
-            <div className="book-title">To Kill a Mockingbird</div>
-            <div className="book-authors">Harper Lee</div>
+            <div className="book-title">{bookData.title}</div>
+            <div className="book-authors">{bookData.authors[0]}</div>
         </div>
     )
 }
