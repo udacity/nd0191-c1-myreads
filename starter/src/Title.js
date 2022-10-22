a36d6888aae3c9331df2679465bd70e2ba8727c5
mostafamaher1990 <mostafamaher1990@gmail.com>
2022-10-22 10:41:03 +0200

    new files added

---
+++ starter/src/Title.js
@@ -0,0 +1,9 @@
+const Title =()=> {
+    return <div className="list-books-title">
+        <h1 className="list-books-title" >
+            MyReads
+        </h1>
+    </div>
+}
+
+export default Title
