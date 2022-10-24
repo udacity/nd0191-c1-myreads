9e3a45baa7ecd376fc1bd19e899caa386e656ee5
Yotam Granada <yotam.granada@gmail.com>
2022-10-24 15:11:35 +0300

    get input in search component

--- starter/src/components/search/Search.jsx
+++ starter/src/components/search/Search.jsx
@@ -1,33 +1,41 @@
 import { search } from "../../BooksAPI";
+import { useState, useEffect } from "react";
 
-const Search = ({showSearchPage, setShowSearchpage}) => {
+const Search = ({ showSearchPage, setShowSearchpage }) => {
+  const [searchInput, setSearchInput] = useState("");
 
-    const searchBooks = () => {
+  const searchBooks = (input) => {
+   
+    setSearchInput(input);
+    
+  };
 
-    }
+  useEffect(()=>{
+    console.log("was changed", searchInput)
+  },[searchInput])
 
-    return ( 
-        <div className="search-books">
-        <div className="search-books-bar">
-          <a
-            className="close-search"
-            onClick={() => setShowSearchpage(!showSearchPage)}
-          >
-            Close
-          </a>
-          <div className="search-books-input-wrapper">
-            <input
-              type="text"
-              placeholder="Search by title, author, or ISBN"
-            //   onChange={searchBooks(input)}
-            />
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid"></ol>
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <a
+          className="close-search"
+          onClick={() => setShowSearchpage(!showSearchPage)}
+        >
+          Close
+        </a>
+        <div className="search-books-input-wrapper">
+          <input
+            type="text"
+            placeholder="Search by title, author, or ISBN"
+            onChange={(e) => searchBooks(e.target.value)}
+          />
         </div>
       </div>
-     );
-}
- 
+      <div className="search-books-results">
+        <ol className="books-grid"></ol>
+      </div>
+    </div>
+  );
+};
+
 export default Search;
