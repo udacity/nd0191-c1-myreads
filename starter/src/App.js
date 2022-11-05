15cf926188a0cb8e8e28768cce87e0c46b5d28dc
Yotam Granada <yotam.granada@gmail.com>
2022-11-05 21:15:45 +0200

    delete commented code

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
-// import { useContext } from "react";
 import Search from "./components/search/Search.js";
 import Main from "./components/main/Main";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { BooksProvider } from "./BooksContext";
-// import { BooksContext } from "./BooksContext";
 
 function App() {
-  // const {readingLists, setReadingLists} = useContext(BooksContext);
-
   return (
     <BooksProvider>
       <BrowserRouter>
