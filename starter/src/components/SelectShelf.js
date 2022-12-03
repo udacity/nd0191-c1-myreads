304949e95d880c023bb8564298f127174ace679c
iabrar <abrar.ksu@gmail.com>
2022-12-03 13:14:13 +0300

    feat: create select shelf component

---
+++ starter/src/components/SelectShelf.js
@@ -0,0 +1,14 @@
+const SelectShelf = () => {
+  return (
+    <select>
+      <option value="none" disabled>
+        Move to...
+      </option>
+      <option value="currentlyReading">Currently Reading</option>
+      <option value="wantToRead">Want to Read</option>
+      <option value="read">Read</option>
+      <option value="none">None</option>
+    </select>
+  );
+};
+export default SelectShelf;
