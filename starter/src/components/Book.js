96361746650859008b4c7f536c28cd60e687e7fd
iabrar <abrar.ksu@gmail.com>
2022-12-03 13:21:32 +0300

    fix: missing react import

--- starter/src/components/Book.js
+++ starter/src/components/Book.js
@@ -1,26 +1,25 @@
+import React from "react";
 import SelectShelf from "./SelectShelf";
 
 const Book = (book) => {
-    return (
-        <div className="book">
-        <div className="book-top">
-          <div
-            className="book-cover"
-            style={{
-              width: 128,
-              height: 188,
-              backgroundImage:
-              `url(${book.coverImgUrl})`,
-            }}
-          ></div>
-          <div className="book-shelf-changer">
-            <SelectShelf/>
-          </div>
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            width: 128,
+            height: 188,
+            backgroundImage: `url(${book.coverImgUrl})`,
+          }}
+        ></div>
+        <div className="book-shelf-changer">
+          <SelectShelf />
         </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.auther}</div>
       </div>
-    );
-  };
-  export default Book;
-  
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">{book.auther}</div>
+    </div>
+  );
+};
+export default Book;
--- starter/src/components/BookShelf.js
+++ starter/src/components/BookShelf.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Book from "./Book";
 
 const BookShelf = () => {
