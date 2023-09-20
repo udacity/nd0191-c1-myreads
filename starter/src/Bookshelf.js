8a8829e30fb2879ad554cc6e60efb7d342b00d1b
Ben Schoen <benshoe@gmail.com>
2023-09-20 17:08:34 +0200

    feat: show books on the correct bookshelf

--- starter/src/Bookshelf.js
+++ starter/src/Bookshelf.js
@@ -2,11 +2,19 @@ import Book from "./Book";
 
 const Bookshelf = ({books, category}) => {
 
+    const isShelf = (book) => {
+        return book.shelf === 'currentlyReading' && category === 'Currently Reading'
+            || book.shelf === 'read' && category === 'Read'
+            || book.shelf == 'wantToRead' && category === 'Want to Read';
+    }
+
     return (
         <div className="bookshelf">
             <div className="bookshelf-title">{category}</div>
             <ol className="books-grid">
-                {books.map((book) =>
+                {books
+                    .filter((book) => isShelf(book))
+                    .map((book) =>
                     <li key={book.id}>
                         <Book book={book}/>
                     </li>
