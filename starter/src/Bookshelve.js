bac23343085b81edbbe0209cb6468cb45ca5abef
Ben Schoen <benshoe@gmail.com>
2023-09-20 11:42:29 +0200

    feat: getting books from the booksAPI and storing them in the app
    additionally added a Bookshelve for presenting something on the page

---
+++ starter/src/Bookshelve.js
@@ -0,0 +1,10 @@
+const Bookshelve = ({category}) => {
+
+    return (
+        <div className="bookshelf">
+            <div className="bookshelf-title">{category}</div>
+        </div>
+    )
+}
+
+export default Bookshelve;
