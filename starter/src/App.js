5f3f86071e08eb245e56626401f61d0b87064681
Ben Schoen <benshoe@gmail.com>
2023-09-23 21:44:54 +0200

    feat: move books to another bookshelf
    - css adjusted for the select-image
    - books are stored in their own shelf array
    - moveBookToShelf method introduced

--- starter/src/App.js
+++ starter/src/App.js
@@ -5,26 +5,48 @@ import Bookshelf from "./Bookshelf";
 
 function App() {
 
-    const [books, setBooks] = useState([]);
+    const [booksCurrentlyReading, setBooksCurrentlyReading] = useState([]);
+    const [booksWantToRead, setbooksWantToRead] = useState([]);
+    const [booksRead, setbooksRead] = useState([]);
 
     useEffect(() => {
         const getBooks = async () => {
             const resp = await BooksAPI.getAll();
             console.log(resp);
-            setBooks(resp);
+            setBooksCurrentlyReading(resp.filter(book => book.shelf === 'currentlyReading'));
+            setbooksWantToRead(resp.filter(book => book.shelf === 'wantToRead'));
+            setbooksRead(resp.filter(book => book.shelf === 'read'));
         };
 
         getBooks();
     }, []);
+
+    const moveBookToShelf = (book, shelf) => {
+        setBooksCurrentlyReading(booksCurrentlyReading.filter(b => b.id !== book.id));
+        setbooksWantToRead(booksWantToRead.filter(b => b.id !== book.id));
+        setbooksRead(booksRead.filter(b => b.id !== book.id));
+
+        book.shelf = shelf;
+        if (shelf === 'currentlyReading') {
+            setBooksCurrentlyReading([...booksCurrentlyReading, book]);
+        }
+        if (shelf === 'wantToRead') {
+            setbooksWantToRead([...booksWantToRead, book]);
+        }
+        if (shelf === 'read') {
+            setbooksRead([...booksRead, book]);
+        }
+    }
 
     return (
         <div className="app">
             <div className="list-books-title">
                 <h1>MyReads</h1>
             </div>
-            <Bookshelf books={books} category="Currently Reading"></Bookshelf>
-            <Bookshelf books={books} category="Want to Read"></Bookshelf>
-            <Bookshelf books={books} category="Read"></Bookshelf>
+            <Bookshelf books={booksCurrentlyReading} onMoveBook={moveBookToShelf}
+                       category="Currently Reading"></Bookshelf>
+            <Bookshelf books={booksWantToRead} onMoveBook={moveBookToShelf} category="Want to Read"></Bookshelf>
+            <Bookshelf books={booksRead} onMoveBook={moveBookToShelf} category="Read"></Bookshelf>
         </div>
     );
 }
--- starter/src/Bookshelf.js
+++ starter/src/Bookshelf.js
@@ -1,22 +1,14 @@
 import Book from "./Book";
 
-const Bookshelf = ({books, category}) => {
-
-    const isShelf = (book) => {
-        return (book.shelf === 'currentlyReading' && category === 'Currently Reading')
-            || (book.shelf === 'read' && category === 'Read')
-            || (book.shelf === 'wantToRead' && category === 'Want to Read');
-    }
+const Bookshelf = ({books, onMoveBook, category}) => {
 
     return (
         <div className="bookshelf">
             <div className="bookshelf-title">{category}</div>
             <ol className="books-grid">
-                {books
-                    .filter((book) => isShelf(book))
-                    .map((book) =>
+                {books.map((book) =>
                         <li key={book.id}>
-                            <Book book={book}/>
+                            <Book book={book} onMoveBook={onMoveBook}/>
                         </li>
                     )}
             </ol>
